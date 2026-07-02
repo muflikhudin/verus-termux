aadd8ff1eea3a679b6216073b0d6ecb2b19ef783
muflikhudin <83884746+muflikhudin@users.noreply.github.com>
2026-07-02 22:55:40 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,21 @@
+#!/data/data/com.termux/files/usr/bin/sh
 
+clear
+
+echo "========================================="
+echo "      VERUS TERMUX UNINSTALLER"
+echo "========================================="
+echo
+
+read -p "Yakin ingin menghapus Verus Termux? (y/N): " JAWAB
+
+if [ "$JAWAB" = "y" ] || [ "$JAWAB" = "Y" ]; then
+    rm -rf ~/ccminer
+    echo
+    echo "========================================="
+    echo "Verus Termux berhasil dihapus."
+    echo "========================================="
+else
+    echo
+    echo "Dibatalkan."
+fi
